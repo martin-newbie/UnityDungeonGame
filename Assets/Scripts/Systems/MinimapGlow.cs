10fe1bbdf5837b25502654de71869a37b8406129
martin-newbie <g.martin.min@gmail.com>
2021-05-09 14:59:33 +0900

    add minimap glow
    the glow will apear when player is in that point

---
+++ Assets/Scripts/Systems/MinimapGlow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapGlow : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            renderer.material.color = Color.yellow;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            renderer.material.color = Color.white;
+            Debug.Log("exit");
+        }
+    }
+}
