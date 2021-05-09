5af4dc8e35c814e074519c885c81405a3ab93027
martin-newbie <g.martin.min@gmail.com>
2021-05-09 10:35:51 +0900

    add minimap
    add minimap camera and renderer

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        if(moveX < 0)
+        {
+            sprite.flipX = true;
+        }
+        if(moveX > 0)
+        {
+            sprite.flipX = false;
+        }
+
+        transform.Translate(moveX * moveSpeed * Time.deltaTime, moveY * moveSpeed * Time.deltaTime, 0);
+
+    }
+}
